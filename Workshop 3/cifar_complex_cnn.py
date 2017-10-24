69cc7b9d7ce8ccc18160a488efa32d9b41f16027
jzharris <james.harris@student.nmt.edu>
2017-10-24 10:48:14 -0600

    Cleaning up Workshop 3

--- Workshop 3/cifar_complex_cnn.py
+++ Workshop 3/cifar_complex_cnn.py
@@ -2,6 +2,10 @@
 ## https://machinelearningmastery.com/object-recognition-convolutional-neural-networks-keras-deep-learning-library/
 
 # Simple CNN model for CIFAR-10
+
+import os
+import os.path as path
+
 import numpy
 from keras.datasets import cifar10
 from keras.models import Sequential
@@ -15,68 +19,107 @@ from keras.layers.convolutional import MaxPooling2D
 from keras.utils import np_utils
 from keras import backend as K
 
-import os
-import os.path as path
+# Compile model
+epochs = 1
+lrate = 0.01
 
+# Simply orders the dataset for 'channels (3) first'
 K.set_image_dim_ordering('th')
 
 # fix random seed for reproducibility
 seed = 7
 numpy.random.seed(seed)
 
-# load data
-(X_train, y_train), (X_test, y_test) = cifar10.load_data()
 
-# normalize inputs from 0-255 to 0.0-1.0
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train = X_train / 255.0
-X_test = X_test / 255.0
+def load_data():
+    (X_train, y_train), (X_test, y_test) = cifar10.load_data()
 
-# one hot encode outputs
-y_train = np_utils.to_categorical(y_train)
-y_test = np_utils.to_categorical(y_test)
-num_classes = y_test.shape[1]
+    # normalize inputs from 0-255 to 0.0-1.0
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+    X_train = X_train / 255.0
+    X_test = X_test / 255.0
 
-# Create the model
-model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=(3, 32, 32), activation='relu', padding='same'))
-model.add(Dropout(0.2))
-model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-model.add(Dropout(0.2))
-model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-model.add(Dropout(0.2))
-model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Flatten())
-model.add(Dropout(0.2))
-model.add(Dense(1024, activation='relu', kernel_constraint=maxnorm(3)))
-model.add(Dropout(0.2))
-model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
-model.add(Dropout(0.2))
-model.add(Dense(num_classes, activation='softmax'))
-# Compile model
-epochs = 25
-lrate = 0.01
-decay = lrate/epochs
-sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-print(model.summary())
+    # one hot encode outputs
+    y_train = np_utils.to_categorical(y_train)
+    y_test = np_utils.to_categorical(y_test)
 
-if path.exists('out/cifar_complex_cnn.h5'):
-    model.load_weights('out/cifar_complex_cnn.h5')
+    return X_train, y_train, X_test, y_test
 
-# Fit the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=64)
-# Final evaluation of the model
-scores = model.evaluate(X_test, y_test, verbose=0)
-print("Accuracy: %.2f%%" % (scores[1]*100))
 
-if not path.exists('out'):
-    os.mkdir('out')
+def build_model(num_classes):
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), input_shape=(3, 32, 32), activation='relu', padding='same'))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dropout(0.2))
+    model.add(Dense(1024, activation='relu', kernel_constraint=maxnorm(3)))
+    model.add(Dropout(0.2))
+    model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
+    model.add(Dropout(0.2))
+    model.add(Dense(num_classes, activation='softmax'))
 
-model.save_weights('out/cifar_complex_cnn.h5')
+    return model
+
+
+def train(model, X_train, y_train, X_test, y_test):
+    decay = lrate/epochs
+    sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    print(model.summary())
+
+    # Fit the model
+    model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=64)
+
+    return model
+
+
+def evaluate(model, X_test, y_test):
+    # Final evaluation of the model
+    scores = model.evaluate(X_test, y_test, verbose=0)
+    print("Accuracy: %.2f%%" % (scores[1]*100))
+
+
+def load(model, filename):
+    if not path.exists('out'):
+        os.mkdir('out')
+
+    if path.exists(filename):
+        model.load_weights(filename)
+
+    return model
+
+
+def save(model, filename):
+    if not path.exists('out'):
+        os.mkdir('out')
+
+    model.save_weights(filename)
+
+
+def main():
+    filename = 'out/cifar_complex_cnn.h5'
+
+    X_train, y_train, X_test, y_test = load_data()
+
+    model = load(build_model(y_test.shape[1]), filename)
+
+    model = train(model, X_train, y_train, X_test, y_test)
+
+    evaluate(model, X_test, y_test)
+
+    save(model, filename)
+
+
+if __name__ == '__main__':
+    main()
--- Workshop 3/cifar_simple_cnn.py
+++ Workshop 3/cifar_simple_cnn.py
@@ -2,6 +2,7 @@
 ## https://machinelearningmastery.com/object-recognition-convolutional-neural-networks-keras-deep-learning-library/
 
 # Simple CNN model for CIFAR-10
+
 import numpy
 from keras.datasets import cifar10
 from keras.models import Sequential
@@ -14,46 +15,74 @@ from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional import MaxPooling2D
 from keras.utils import np_utils
 from keras import backend as K
+
+# Compile model
+epochs = 25
+lrate = 0.01
+
+# Simply orders the dataset for 'channels (3) first'
 K.set_image_dim_ordering('th')
 
 # fix random seed for reproducibility
 seed = 7
 numpy.random.seed(seed)
 
-# load data
-(X_train, y_train), (X_test, y_test) = cifar10.load_data()
 
-# normalize inputs from 0-255 to 0.0-1.0
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train = X_train / 255.0
-X_test = X_test / 255.0
+def load_data():
+    (X_train, y_train), (X_test, y_test) = cifar10.load_data()
 
-# one hot encode outputs
-y_train = np_utils.to_categorical(y_train)
-y_test = np_utils.to_categorical(y_test)
-num_classes = y_test.shape[1]
+    # normalize inputs from 0-255 to 0.0-1.0
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+    X_train = X_train / 255.0
+    X_test = X_test / 255.0
 
-# Create the model
-model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=(3, 32, 32), padding='same', activation='relu', kernel_constraint=maxnorm(3)))
-model.add(Dropout(0.2))
-model.add(Conv2D(32, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Flatten())
-model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
-# Compile model
-epochs = 25
-lrate = 0.01
-decay = lrate/epochs
-sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-print(model.summary())
+    # one hot encode outputs
+    y_train = np_utils.to_categorical(y_train)
+    y_test = np_utils.to_categorical(y_test)
 
-# Fit the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=32)
-# Final evaluation of the model
-scores = model.evaluate(X_test, y_test, verbose=0)
-print("Accuracy: %.2f%%" % (scores[1]*100))
+    return X_train, y_train, X_test, y_test
+
+
+def build_model(num_classes):
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), input_shape=(3, 32, 32), padding='same', activation='relu', kernel_constraint=maxnorm(3)))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    return model
+
+
+def train(model, X_train, y_train, X_test, y_test):
+    decay = lrate/epochs
+    sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    print(model.summary())
+
+    # Fit the model
+    model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=32)
+
+
+def evaluate(model, X_test, y_test):
+    # Final evaluation of the model
+    scores = model.evaluate(X_test, y_test, verbose=0)
+    print("Accuracy: %.2f%%" % (scores[1]*100))
+
+
+def main():
+    X_train, y_train, X_test, y_test = load_data()
+
+    model = build_model(y_test.shape[1])
+
+    train(model, X_train, y_train, X_test, y_test)
+
+    evaluate(model, X_test, y_test)
+
+
+if __name__ == '__main__':
+    main()
