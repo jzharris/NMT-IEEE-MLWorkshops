9168f28afe8a897e9a0e88aa6250376e1a21fa90
jzharris <james.harris@student.nmt.edu>
2017-10-23 23:15:55 -0600

    Adding template to Workshop 3

---
+++ Workshop 3/plot_cifar.py
@@ -0,0 +1,16 @@
+# Required packages:
+# pip3 install matplotlib
+# sudo apt-get install python3-tk
+
+# Plot ad hoc CIFAR10 instances
+from keras.datasets import cifar10
+from matplotlib import pyplot
+from scipy.misc import toimage
+# load data
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+# create a grid of 3x3 images
+for i in range(0, 9):
+	pyplot.subplot(330 + 1 + i)
+	pyplot.imshow(toimage(X_train[i]))
+# show the plot
+pyplot.show()
