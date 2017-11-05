9559bf860633d3900761025fb73b77c7d51fd301
jzharris <james.harris@student.nmt.edu>
2017-11-04 19:11:56 -0600

    Finished adding first demo

--- Workshop 5/demo1.py
+++
@@ -1,9 +0,0 @@
-# Code taken from video: https://www.youtube.com/watch?v=ftMq5ps503w
-
-from keras.layers.core import Dense, Activation, Dropout
-from keras.layers.recurrent import LSTM
-from keras.models import Sequential
-import lstm, time # helper libraries
-
-x_train, y_train, x_test, y_test = lstm.load_data('sp500.csv', 50, True)
-
