31dd061cba4ec46089b74f565baa2f74d91964fe
jzharris <james.harris@student.nmt.edu>
2017-11-08 23:22:40 -0700

    Editing Workshop 5

--- Workshop 5/Shakespeare/rnn3.py
+++ Workshop 5/Shakespeare/rnn3.py
@@ -115,9 +115,9 @@ def load_weights(model):
 def build_model(X, y):
     # define the LSTM model
     model = Sequential()
-    model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
+    model.add(LSTM(512, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
     model.add(Dropout(0.2))
-    model.add(LSTM(256))
+    model.add(LSTM(512))
     model.add(Dropout(0.2))
     model.add(Dense(y.shape[1], activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam')
